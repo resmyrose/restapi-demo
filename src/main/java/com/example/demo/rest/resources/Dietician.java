791c506e3507da52c0b401f5b5b1b79119c96c72
Austin Thomas <austin.thomas@Austins-Mac-mini.local>
2024-08-22 21:19:49 -0700

    initial commit

---
+++ src/main/java/com/example/demo/rest/resources/Dietician.java
@@ -0,0 +1,16 @@
+package com.example.demo.rest.resources;
+
+public class Dietician {
+
+	private Long   id;
+	private String firstName;
+	private String lastName;
+	private String contact;
+	private String dateOfBirth;
+	private String userLoginEmail;
+	private String hospitalName;
+	private String hospitalStreet;
+	private String hospitalCity;
+	private String hospitalPinCode;
+	private String education;
+}
---
+++ src/main/java/com/example/demo/rest/resources/Patient.java
@@ -0,0 +1,14 @@
+package com.example.demo.rest.resources;
+
+public class Patient {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String contactNumber;
+	private String email;
+	private String allergy;
+	private String foodPreference;
+	private String CuisineCategory;
+	
+}
